24ef63e2079b9161ddf41436c3a96b1a374e0fd9
daATva <riven1231@mail.ru>
2024-03-01 20:22:34 +0400

    add modal window

--- src/components/QuestionDetails/QuestionDetails.jsx
+++ src/components/QuestionDetails/QuestionDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { questions } from '../../data/questions';
 import './QuestionDetails.scss';
@@ -11,6 +11,9 @@ const QuestionDetails = () => {
     return <div>Question not found</div>;
   }
 
+  // Создаем состояние для показа ответа
+  const [showAnswer, setShowAnswer] = useState(false);
+
   // Просто заглушки для демонстрации
   const handleKnowClick = () => {
     console.log('Ответ знаю:', question.id);
@@ -22,19 +25,21 @@ const QuestionDetails = () => {
     // Здесь можно добавить логику для обработки ответа "Не знаю"
   };
 
+  // Изменяем состояние на противоположное при клике на кнопку
   const handleAnswerClick = () => {
-    console.log('Ответ не знаю:', question.id);
-    // Здесь можно добавить логику для обработки ответа "Не знаю"
+    console.log('Посмотреть ответ:', question.id);
+    setShowAnswer(!showAnswer);
   };
 
   return (
     <div className="question-details">
       <h2>{question.question}</h2>
-      <p>{question.answer}</p>
-      <div>
-        <button onClick={handleKnowClick}>Знаю</button>
-        <button onClick={handleDontKnowClick}>Не знаю</button>
-        <button onClick={handleAnswerClick}>Посмотреть ответ</button>
+      {/* Показываем параграф с ответом только если showAnswer равно true */}
+      {showAnswer && <p>{question.answer}</p>}
+      <div> 
+        <button className='know-button' onClick={handleKnowClick}>Знаю</button>
+        <button className='dont-know-button' onClick={handleDontKnowClick}>Не знаю</button>
+        <button className='show-answer-button' onClick={handleAnswerClick}>Посмотреть ответ</button>
       </div>
     </div>
   );
--- src/store/store/store.jsx
+++ src/store/store/store.jsx
@@ -1,11 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk'; // Исправлено для использования именованного импорта
 import rootReducer from '../reducers';
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
-import { thunk } from 'redux-thunk';
 
- const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)});
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk) // Используйте именованный импорт
+);
 
 console.log(store);
-export default store;
 
+export default store;
